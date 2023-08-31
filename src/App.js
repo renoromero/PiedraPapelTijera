86479ffc178e1101449343a268f158e3b8c9a217
Renato Romero <deltacar@gmail.com>
2023-08-31 07:59:35 -0300

    Seleccion Rival coompleta

--- src/App.js
+++ src/App.js
@@ -4,19 +4,28 @@ import Rival from './componentes/Rival';
 import papel from './imagenes/papel.png';
 import piedra from './imagenes/piedra.png';
 import tijera from './imagenes/tijera.png';
+import { useState } from 'react';
 
 
 function App() {
 
-var selectRival = [tijera];
+  const [numbersArray, setNumbersArray] = useState([]);
+
+  const generateRandomNumber = () => {
+    const randomNumber = Math.floor(Math.random() * 3); // Genera un número aleatorio entre 0 y 99
+    setNumbersArray([randomNumber]); // Agrega el número al arreglo existente
+  }
+
+var selectRival = [tijera, papel, piedra];
+
   return (
     <div className="App">
       <div className='botones-contenedor'>
         <Player/>
-        
+      <button onClick={generateRandomNumber}>1</button>
       </div>
      <Rival
-     imagen={selectRival}/>
+     imagen={selectRival[numbersArray]}/>
     </div>
   );
 }
